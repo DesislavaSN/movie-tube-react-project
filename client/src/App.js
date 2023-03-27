6c5dcd3648565d6e355eecd6d82e1425ff75cec7
DESISLAVA Stefanova NEDEVA <65559215+DesislavaSN@users.noreply.github.com>
2023-03-27 15:43:55 +0100

    Create App.js

---
+++ client/src/App.js
@@ -0,0 +1,42 @@
+import { Routes, Route } from 'react-router-dom';
+
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import Home from './components/Home/Home';
+import Catalog from './components/Catalog/Catalog';
+import Register from './components/Register/Register';
+import Login from './components/Login/Login';
+import CreateMovie from './components/CreateMovie/CreateMovie';
+import Details from './components/Details/Details';
+import EditMovie from './components/EditMovie/EditMovie';
+import Logout from './components/Logout/Logout';
+
+import { AuthProvider } from './contexts/AuthContext';
+
+function App() {
+  return (
+    <AuthProvider>
+      <div id="wrapper">
+        <Header />
+
+        <main>
+          <Routes>
+            <Route></Route>
+            <Route path='/' element={<Home />} />
+            <Route path='/catalog' element={<Catalog />} />
+            <Route path='/register' element={<Register />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/logout' element={<Logout />} />
+            <Route path='/create-movie' element={<CreateMovie />} />
+            <Route path='/catalog/:movieId' element={<Details />} />
+            <Route path='/catalog/:movieId/edit' element={<EditMovie />} />
+          </Routes>
+        </main>
+
+      </div>
+      <Footer />
+    </AuthProvider>
+  );
+}
+
+export default App;
