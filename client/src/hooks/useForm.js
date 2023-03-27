8ae0ec300cf0bab2ce61b62d0e84b5c3bb3fe9af
DESISLAVA Stefanova NEDEVA <65559215+DesislavaSN@users.noreply.github.com>
2023-03-27 15:58:25 +0100

    Create useForm.js

---
+++ client/src/hooks/useForm.js
@@ -0,0 +1,28 @@
+import { useState } from 'react';
+
+export default function useForm(initialValues, onSubmitHandler) {
+    const [formValues, setFormValues] = useState(initialValues);
+
+    function onChangeHandler (e) {
+        setFormValues(state => ({...state, [e.target.name]: e.target.value}));
+    }
+
+    function onSubmit(e) {
+        e.preventDefault();
+        onSubmitHandler(formValues);
+        setFormValues(initialValues);
+    }
+
+    function onChangeValues(newValues) {
+        // TODO: Validate newValues shape (like initialValues);
+
+        setFormValues(newValues);
+    }
+
+    return {
+        formValues,
+        onChangeHandler,
+        onSubmit,
+        onChangeValues
+    };
+}
