cbe48c2c381b33b4883e1688bde36be6bafe1383
DESISLAVA Stefanova NEDEVA <65559215+DesislavaSN@users.noreply.github.com>
2023-03-28 15:16:36 +0100

    Update movieService.js

--- client/src/services/movieService.js
+++ client/src/services/movieService.js
@@ -2,35 +2,38 @@ import { requestFactory } from './requester';
 
 const baseUrl = 'http://localhost:3030/data/movies';
 
-const request = requestFactory();
+export const movieServiceFactory = (token) => {
+    const request = requestFactory(token);
 
-async function getAllMovies(){
-    const result = await request.get(baseUrl);
-    const movies = Object.values(result);
-    return movies;
+    async function getAllMovies() {
+        const result = await request.get(baseUrl);
+        const movies = Object.values(result);
+        return movies;
+    }
+
+    async function getById(id) {
+        const result = await request.get(`${baseUrl}/${id}`);
+        // console.log(result);
+        return result;
+    }
+
+    async function create(data) {
+        const result = await request.post(baseUrl, data);
+        // console.log(result);
+        return result;
+    }
+
+    // async function addComment(movieId, data) {
+    //     const result = await request.post(`${baseUrl}/${movieId}/comments`, data);
+    //     return result;
+    // }
+
+    return {
+        getAllMovies,
+        getById,
+        create,
+        // addComment,
+
+    };
+
 }
-
-async function getById(id) {
-    const result = await request.get(`${baseUrl}/${id}`);
-    // console.log(result);
-    return result;
-}
-
-async function create (data) {
-    const result = await request.post(baseUrl, data);
-    // console.log(result);
-    return result;
-}
-
-async function addComment(movieId, data) {
-    const result = await request.post(`${baseUrl}/${movieId}/comments`, data);
-    return result;
-}
-
-export {
-    getAllMovies,
-    getById,
-    create,
-    addComment,
-    
-};
