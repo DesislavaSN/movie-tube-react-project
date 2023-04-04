1d1fe369451ae0291fd1524710334b42d7be1229
DESISLAVA Stefanova NEDEVA <65559215+DesislavaSN@users.noreply.github.com>
2023-04-04 12:27:07 +0100

    Create RouteGuard.js

---
+++ client/src/common/RouteGuard.js
@@ -0,0 +1,10 @@
+import { Navigate, Outlet } from 'react-router-dom';
+import { useAuthContext } from '../contexts/AuthContext';
+
+export default function RouteGuard ({ children }) {
+    const { isAuthenticated } = useAuthContext();
+    if (!isAuthenticated) {
+        return <Navigate to='/login' />;
+    }
+    return children ? children : <Outlet />;
+}
