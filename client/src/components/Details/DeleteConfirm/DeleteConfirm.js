ce0ea9add49829ce848aed071f9172afc412bc46
DESISLAVA Stefanova NEDEVA <65559215+DesislavaSN@users.noreply.github.com>
2023-04-04 12:35:59 +0100

    Update DeleteConfirm.js

--- client/src/components/Details/DeleteConfirm/DeleteConfirm.js
+++ client/src/components/Details/DeleteConfirm/DeleteConfirm.js
@@ -1,3 +1,4 @@
+import styles from './DeleteConfirm.module.css';
 
 export default function DeleteConfirm({
     movie,
@@ -5,11 +6,11 @@ export default function DeleteConfirm({
     onClose
 }) {
     return (
-        <section id="del-confirm">
-            <div className="form del-confirm-modul">
+        <section id={styles.delConfirm}>
+            <div className={styles.form}>
                 <h4>Are you sure you want to delete <span>{movie.title}</span> movie?</h4>
-                <button className="conf-btn del" type="submit" onClick={onDeleteMovie}>Delete</button>
-                <button className="conf-btn cancel" type="submit" onClick={onClose}>Cancel</button>
+                <button className={styles.confBtnDel} type="submit" onClick={onDeleteMovie}>Delete</button>
+                <button className={styles.confBtnCancel} type="submit" onClick={onClose}>Cancel</button>
             </div>
         </section>
     );
