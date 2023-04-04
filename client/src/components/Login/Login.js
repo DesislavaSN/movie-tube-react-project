523a6fb19ef6954fc4c3f721a1edf30a1ec160db
DESISLAVA Stefanova NEDEVA <65559215+DesislavaSN@users.noreply.github.com>
2023-04-04 12:42:22 +0100

    Update Login.js

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -1,29 +1,71 @@
 import { Link } from 'react-router-dom';
+import styles from './Login.module.css';
 
 import { useAuthContext } from '../../contexts/AuthContext';
 import useForm from '../../hooks/useForm';
+import { useState } from 'react';
 
 export default function Login() {
+
 
     const { onLoginSubmit } = useAuthContext();
     const {formValues, onChangeHandler, onSubmit} = useForm({
         email: '',
         password: ''
     }, onLoginSubmit);
+ 
+    const [error, setError] = useState({
+        username: '',
+        email: '',
+        password: '',
+        confirmPassword: ''
+    });
+
+    const validateFormValues = (e) => {
+        const { name, value } = e.target;
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,3}$/i;
+
+        setError(state => {
+            const stateObj = { ...state, [name]: '' };
+
+            switch (name) {
+                case 'email':
+                    if (!emailRegex.test(value)) {
+                        stateObj[name] = 'Please eneter a valid email.';
+                    }
+                    break;
+                case 'password':
+                    if (!value) {
+                        stateObj[name] = 'Please eneter a valid password.';
+                    }
+                    break;
+                
+                default:
+                    break;
+            }
+
+            return stateObj;
+        });
+    };
+
+    const isValid = !Object.values(error).some(x => x);
 
     return (
-        <section id="login">
-            <div className="form">
+        <section id={styles.login}>
+            <div className={styles.form}>
                 <h2>Login</h2>
-                <form className="login-form" onSubmit={onSubmit}>
+                <form onSubmit={onSubmit}>
+                    {error.email && <span className={styles.err}>{error.email}</span>}
                     <input 
                         type="text" 
                         id="email" 
                         name="email"
-                        placeholder="yourEmail@gmail.com" 
+                        placeholder="email@gmail.com" 
                         value={formValues.email}
                         onChange={onChangeHandler}
+                        onBlur={validateFormValues}
                     />
+                    {error.password && <span className={styles.err}>{error.password}</span>}
                     <input 
                         type="password" 
                         name="password" 
@@ -31,10 +73,11 @@ export default function Login() {
                         placeholder="password" 
                         value={formValues.password}
                         onChange={onChangeHandler}
+                        onBlur={validateFormValues}
                     />
-                    <button type="submit">login</button>
-                    <p className="message">
-                        Not registered? <Link to="/register">Create an account</Link>
+                    <button type="submit" disabled={!isValid}>login</button>
+                    <p className={styles.message}>
+                        Not registered? <Link to="/register" className={styles.link}>Create an account</Link>
                     </p>
                 </form>
             </div>
