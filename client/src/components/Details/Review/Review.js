df7436939c3244eb55b189b85167cf4533d7b602
DESISLAVA Stefanova NEDEVA <65559215+DesislavaSN@users.noreply.github.com>
2023-04-04 10:41:46 +0100

    Update Review.js

--- client/src/components/Details/Review/Review.js
+++ client/src/components/Details/Review/Review.js
@@ -1,12 +1,35 @@
 
-export default function Review() {
+import useForm from '../../../hooks/useForm';
+import styles from '../Details.module.css';
+
+export default function Review({onReviewSubmit}) {
+    const {formValues, onChangeHandler, onSubmit} = useForm({
+        username: '',
+        comments: ''
+    },onReviewSubmit);
+
     return (
-        <section id="comments">
-            <div className="form review-form">
+        <section id={styles.comments}>
+            <div className={styles.form}>
                 <h2>Leave your review</h2>
-                <form className="login-form">
-                    <input type="text" name="username" id="username" placeholder="Name" />
-                    <textarea id="description" name="description" placeholder="Review" rows="4" cols="50"></textarea>
+                <form onSubmit={onSubmit}>
+                    <input 
+                        type="text" 
+                        name="username" 
+                        id="username" 
+                        placeholder="Name" 
+                        value={formValues.username} 
+                        onChange={onChangeHandler}   
+                    />
+                    <textarea 
+                        id="comments" 
+                        name="comments" 
+                        placeholder="Review" 
+                        rows="4" 
+                        cols="50"
+                        value={formValues.comments} 
+                        onChange={onChangeHandler}  
+                    ></textarea>
                     <button type="submit">Submit</button>
                 </form>
             </div>
