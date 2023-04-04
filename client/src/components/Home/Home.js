60419830bf01b1029c10b04114ceb6596478baf6
DESISLAVA Stefanova NEDEVA <65559215+DesislavaSN@users.noreply.github.com>
2023-04-04 12:41:37 +0100

    Update Home.js

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,6 +1,8 @@
+import styles from './Home.module.css';
+
 export default function Home() {
     return (
-      <section id="home">
+      <section id={styles.home}>
         <img src="/images/pic2.PNG" alt="homePic2" />
       </section>
     );
