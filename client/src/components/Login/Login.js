32e22c18971bb0ff53d2f315d5f18fe5082a579b
DESISLAVA Stefanova NEDEVA <65559215+DesislavaSN@users.noreply.github.com>
2023-04-08 12:44:28 +0300

    Update Login.js

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -7,7 +7,6 @@ import { useState } from 'react';
 
 export default function Login() {
 
-
     const { onLoginSubmit } = useAuthContext();
     const {formValues, onChangeHandler, onSubmit} = useForm({
         email: '',
